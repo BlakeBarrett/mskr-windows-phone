524ac78568cec203279981f3f22f33e365b68696
Blake Barrett <blake@blakebarrett.net>
2014-01-11 23:35:42 -0800

    I present to you SAVE!

--- mskr/mskr/MainPage.xaml.cs
+++ mskr/mskr/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
@@ -28,29 +29,62 @@ namespace mskr
         }
 
         // Simple button Click event handler to take us to the second page
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void SelectImageButton_Click(object sender, RoutedEventArgs e)
         {
-            //NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
             photoChooserTask.Show();
+        }
+
+        private void SaveImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            RasterizeStack(PreviewImage);
+            SaveImageGrid.Visibility = Visibility.Collapsed;
+            SelectImageGrid.Visibility = Visibility.Visible;
         }
 
         void photoChooserTask_Completed(object sender, PhotoResult e)
         {
             if (e.TaskResult == TaskResult.OK)
             {
-                //MessageBox.Show(e.ChosenPhoto.Length.ToString());
+                System.IO.Stream selected = e.ChosenPhoto;
 
                 //Code to display the photo on the page in an image control named myImage.
                 System.Windows.Media.Imaging.BitmapImage bmp = new System.Windows.Media.Imaging.BitmapImage();
-                bmp.SetSource(e.ChosenPhoto);
+                bmp.SetSource(selected);
                 PreviewImage.Source = bmp;
 
-                PageTitle.Visibility = Visibility.Collapsed;
-                // TODO: Render full-sized masked image and save that.
+                SelectImageGrid.Visibility = Visibility.Collapsed;
+                SaveImageGrid.Visibility = Visibility.Visible;
+                // TODO: Render full-sized masked image.
                 // Also, offer the ability to "stretch" or "constrain" the mask mode.
                 // it appears as though masks are stretched to fit their parent images by default.
                 // Perhaps the parent image needs to be constrained to the aspect ratio of the mask image at some point.
             }
+        }
+
+        void RasterizeStack(UIElement stack)
+        {
+            WriteableBitmap writeableBitmap = new WriteableBitmap(stack, null);
+            using (System.IO.MemoryStream s = new System.IO.MemoryStream())
+            {
+                writeableBitmap.SaveJpeg(s, (int)PreviewImage.ActualWidth, (int)PreviewImage.ActualHeight, 0, 100);
+                String filename = DateTime.Now.Ticks.ToString() + ".jpg";
+                WriteStreamToFile(s, filename);
+            }
+        }
+
+        void WriteStreamToFile(System.IO.Stream stream, String filename)
+        {
+            Microsoft.Xna.Framework.Media.MediaLibrary library = new Microsoft.Xna.Framework.Media.MediaLibrary();
+
+            // write to "Saved Pictures"
+            stream.Position = 0;
+            library.SavePicture(filename, stream);
+
+            // write to "Camera Roll"
+            //stream.Position = 0;
+            //library.SavePictureToCameraRoll(filename, stream);
+
+            stream.Dispose();
         }
     }
 }
---
+++ mskr/mskr/com/blakebarrett/imaging/MaskedBitmapImage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace mskr.com.blakebarrett.imaging
+{
+    class MaskedBitmapImage
+    {
+        private BitmapImage source;
+        private BitmapImage mask;
+        private Image image;
+
+        public MaskedBitmapImage(BitmapImage source, BitmapImage mask)
+        {
+            this.source = source;
+            this.mask = mask;
+            Render();
+        }
+
+        private void Render()
+        {
+            image = new Image();
+            image.Source = source;
+            ImageBrush brush = new ImageBrush();
+            brush.ImageSource = mask;
+            image.OpacityMask = brush;
+        }
+
+        public void WriteToFile(String filename)
+        {
+
+        }
+    }
+}
